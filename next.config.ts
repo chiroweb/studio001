6259d88e972d0ed0eaafa7df7997f546276e1d1b
최정원 <choejeong-won@choejeong-won-ui-MacBookPro.local>
2026-07-24 01:01:26 +0900

    Prepare static Sites deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   outputFileTracingRoot: process.cwd(),
   images: {
     formats: ["image/avif", "image/webp"],
+    unoptimized: true,
   },
 };
 
